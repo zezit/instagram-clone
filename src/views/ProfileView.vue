cd03e1e01569ced04dfb39951f6e86ad8912f640
José Victor <jvictormmendesd@gmail.com>
2023-05-17 15:47:44 -0300

    começando a criar pagina de perfil do usuário

---
+++ src/views/ProfileView.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { userCredentials } from '../stores/userCred'
+import { storeToRefs } from "pinia"
+
+const userLog = userCredentials()
+const { user } = storeToRefs(userLog)
+
+</script>
+
+<template>
+    <div class="container">
+        <div class="center-container">
+            <h1>Profile</h1>
+        </div>
+        <div class="center-container">
+            <p>Username: {{ user.username }}</p>
+            <p>First Name: {{ user.fName }}</p>
+            <p>Last Name: {{ user.lName }}</p>
+            <p>Email: {{ user.email }}</p>
+        </div>
+
+    </div>
+</template>
+
+<style scoped></style>
